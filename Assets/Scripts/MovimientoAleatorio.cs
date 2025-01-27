27a5fc81e2891ba730342abef804d350efb505de
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-01-27 14:19:06 +0100

    Rotacion asteroides

--- Assets/Scripts/MovimientoAleatorio.cs
+++ Assets/Scripts/MovimientoAleatorio.cs
@@ -8,11 +8,14 @@ public class MovimientoAleatorio : MonoBehaviour
     float horizontal;
     float vertical;
     float speed;
+    float movement_rotation;
     void Start()
     {
         horizontal = Random.Range(-1f, 1f);
         vertical = Random.Range(-1f, 1f);
         speed = Random.Range(1f, 3f);
+        movement_rotation = Random.Range(0f, 360f);
+        transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, movement_rotation );
 
     }
     void Update()
